600f215e05dd14d4d662014f842f3e706054148b
wangjinliang1991 <wjl31802@126.com>
2023-07-30 12:33:57 +0800

    leader/follower模式

--- agent.js
+++ agent.js
@@ -1,8 +1,21 @@
 const Subscriber = require('./lib/subscriber');
+const RegistryClient = require('./lib/registry_client');
 
 module.exports = agent => {
-  const subscriber = new Subscriber();
-  // listen
-  subscriber.on('changed', () =>
-    agent.messenger.sendToApp('refresh', 'push'));
+  agent.registryClient = agent.cluster(RegistryClient).create({});
+
+  agent.beforeStart(async () => {
+    await agent.registryClient.ready();
+    agent.logger.info(`${agent.type} : LEADER ${agent.registryClient.options.isLeader}`);
+
+    const subscriber = new Subscriber();
+    // listen changed event, broadcast to all workers
+    subscriber.on('changed', (data) => agent.registryClient.publish({
+      dataId: 'refresh',
+      publishData: {
+        by: 'push',
+        data,
+      },
+    }));
+  });
 };
--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+const RegistryClient = require('./lib/registry_client');
+
 module.exports = app => {
-  app.messenger.on('refresh', by => {
-    app.logger.info('start update by %s', by);
-    // create an anonymous context to access service
-    const ctx = app.createAnonymousContext();
-    ctx.runInBackground(async () => {
-      await ctx.service.source.update();
-      app.lastUpdateBy = by;
-    });
+  app.registryClient = app.cluster(RegistryClient).create({});
+
+  app.beforeStart(async () => {
+    await app.registryClient.ready();
+    app.logger.info(`${app.type} : LEADER ${app.registryClient.options.isLeader}`);
+
+    app.registryClient.subscribe({
+      dataId: 'refresh'
+    }, reg => {
+      app.logger.info('start update by %s', reg.by);
+      // create an anonymous context to access service
+      const ctx = app.createAnonymousContext()
+      ctx.runInBackground(async ()=>{
+        await ctx.service.source.update(reg.data);
+        app.lastUpdateBy = reg.by;
+      })
+    })
   });
+  
 };
